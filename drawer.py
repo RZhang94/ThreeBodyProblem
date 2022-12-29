cd8fe411a61a79da2d5398b83cb0df7425ec9093
Ray <r.zhang1994@gmail.com>
2022-12-29 13:56:04 -0700

    Integrator and dynamics

--- drawer.py
+++ drawer.py
@@ -5,6 +5,8 @@ class animator():
         self.x_t = x_t
         self.names = names
         self.bounds = self.getBounds()
+        self.fig = plt.figure()
+        self.createHistory()
 
     def getBounds(self):
         lowX1 = min(self.x_t[:,1])
@@ -23,13 +25,14 @@ class animator():
 
         return [min(lowX1, lowX2, lowX3), max(highX1,highX2, highX3), min(lowY1, lowY2, lowY3), max(highY1,highY2, highY3)]
 
+    def createHistory(self):
+        size = self.x_t.shape
+        self.history = np.array()
 
     def plotSingleFrame(self, x):
-        fig = plt.figure()
-
-        plt.scatter(x[1],x[2], color = 'red')
-        plt.scatter(x[5], x[6], color = 'blue')
-        plt.scatter(x[9], x[10], color = 'g')
+        plt.scatter(x[1],x[2], color = 'red', label = self.names[0])
+        plt.scatter(x[5], x[6], color = 'blue', label = self.names[1])
+        plt.scatter(x[9], x[10], color = 'g', label = self.names[2])
 
         titleString = f"Time: {x[0]}"
         plt.title(titleString)
